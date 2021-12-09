0820c5344cc0acce848481320edf4b0078b5ee9d
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-09 20:27:16 +0100

    started css for home mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 const Navbar = () => {
   return (
     <nav className="nav">
-      <div className="nav__container">
+      <div className="nav__center">
         <img src={logo} alt="company-logo" className="nav__img" />
         <GiHamburgerMenu className="nav__hamburger" />
       </div>
